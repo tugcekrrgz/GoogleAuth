65a01fdf7a748afa24624ac86edf8e201bf24797
tugcekrrgz <123090853+tugcekrrgz@users.noreply.github.com>
2023-10-05 16:20:33 +0300

    Google Login

--- GoogleAuth.MVC/Controllers/AuthController.cs
+++ GoogleAuth.MVC/Controllers/AuthController.cs
@@ -1,11 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace GoogleAuth.MVC.Controllers
 {
     public class AuthController : Controller
     {
-        public IActionResult Register()
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager)
         {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            var externalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            return View(externalLogins);
+        }
+
+        [HttpPost]
+        public IActionResult GoogleLogin(string provider)
+        {
+            var redirectUrl = Url.Action("GoogleLoginCallBack","Auth");
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider,redirectUrl);
+            return Challenge(properties,provider);
+        }
+
+        public async Task<IActionResult> GoogleLoginCallBack()
+        {
+            var externalLoginInfo = await _signInManager.GetExternalLoginInfoAsync();
+
+            if(externalLoginInfo == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var email=externalLoginInfo.Principal.FindFirst(ClaimTypes.Email).Value;
+
+            if(email == null) {
+                return RedirectToAction("Login");
+            }
+
+            var user=await _signInManager.UserManager.FindByEmailAsync(email);
+
+            if(user == null)
+            {
+
+            }
+
             return View();
         }
     }
