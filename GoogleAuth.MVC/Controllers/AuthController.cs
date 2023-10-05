ffe98c32ebc61e62f34796ec2c7424cd45e6d9da
tugcekrrgz <123090853+tugcekrrgz@users.noreply.github.com>
2023-10-05 14:29:22 +0300

    Google App

---
+++ GoogleAuth.MVC/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleAuth.MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ GoogleAuth.MVC/Models/GoogleAuthContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoogleAuth.MVC.Models
+{
+    public class GoogleAuthContext:IdentityDbContext
+    {
+        public GoogleAuthContext(DbContextOptions<GoogleAuthContext> options):base(options) { }
+    }
+}
